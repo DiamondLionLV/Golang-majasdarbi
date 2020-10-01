74a98928d386aa56187e6d1bd60d6f0b291efe3b
DiamondLionLV <DiamondLion@ravine.com>
2020-10-01 23:18:25 +0300

    Izlaboju 6uzd.go failu (nepareizi sarekina lenkus AKA nepareiza lenku aprekinasans formula)

--- 6uzd.go
+++ 6uzd.go
@@ -1,19 +1,22 @@
 package main
+
 import "fmt"
 
 func main() {
 	var a, b, c, d, e, f int
 	fmt.Print("Ievadi stundas `(1-12)`")
-	fmt.Scan(a)
+	fmt.Scan(&a)
 	fmt.Print("Ievadi minutes `(1-60)`")
-	fmt.Scan(b)
+	fmt.Scan(&b)
 
-	c := 360 / b
-	d := 60 / a
+	c = 360 / b
+	d = 60 / a
+	e = c - d
+	f = c + d
 
 	if c > d {
-		e := c-d
+		fmt.Print("Lenkis: ", e)
 	} else {
-		f := c+d
+		fmt.Print("Lenkis: ", f)
 	}
 }
