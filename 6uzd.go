3aa17b3aff1a66769e05042a0fade52689f0036e
DiamondLionLV <DiamondLion@ravine.com>
2020-10-02 22:16:02 +0300

    Izlaboju 6uzd.go failu (VISS STRADA)

--- 6uzd.go
+++ 6uzd.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var a, b, c, d, e, f int
+	var a, b int
 	fmt.Print("Ievadi stundas `(1-12)`")
 	fmt.Scan(&a)
 	fmt.Print("Ievadi minutes `(1-60)`")
 	fmt.Scan(&b)
 
-	c = 360 / b
-	d = 60 / a
-	e = c - d
-	f = c + d
+	c := a * 30
+	d := b * 6
 
 	if c > d {
-		fmt.Print("Lenkis: ", e)
+		//fmt.Print("Lenkis: ", e)
+		fmt.Printf("Lenkis: %d", c-d)
 	} else {
-		fmt.Print("Lenkis: ", f)
+		//fmt.Print("Lenkis: ", f)
+		fmt.Printf("Lenkis: %d", d-c)
 	}
 }
